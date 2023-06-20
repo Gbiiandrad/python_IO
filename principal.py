99e2e6b2387c6a41240dbcaf9da87bbdc2df5081
Gabi Desktop <gabii12303.net@gmail.com>
2023-06-20 20:19:42 -0300

    🚧

--- principal.py
+++ principal.py
@@ -1,7 +1,4 @@
 arquivo_contatos = open('dados/contatos.csv', encoding='latin_1')
 
-conteudo = arquivo_contatos.readlines()
-print(conteudo)
-
-for linha in conteudo:
+for linha in arquivo_contatos:
     print(linha, end='')
---
+++ testa_escrita.py
@@ -0,0 +1,7 @@
+arquivo_contatos = open('dados/contatos-escrita.csv',
+                        encoding='latin_1', mode='a')
+
+contato = '11,Carol,carol@carol.com.br\n'
+
+arquivo_contatos.write(contato)
+print(contato)
