12f7e341b3f17365fdf8e2aebf9afe6f5566b685
Gabi Desktop <gabii12303.net@gmail.com>
2023-06-21 16:52:22 -0300

    🚧 about try,finally and except Error

--- principal.py
+++ principal.py
@@ -1,4 +1,14 @@
-arquivo_contatos = open('dados/contatos.csv', encoding='latin_1')
+try:
+    arquivo_contatos = open('dados/contatos.csv', encoding='latin_1'mode='w+')
 
-for linha in arquivo_contatos:
-    print(linha, end='')
+    for linha in arquivo_contatos:
+        print(linha, end='')
+
+except FileNotFoundError:
+    print('Arquivo não encontrado!')
+
+except PermissionError:
+    print('Você não tem permissão de escrita!')
+
+finally:
+    arquivo_contatos.close()
