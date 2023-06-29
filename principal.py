7173e322c3ee507faf2ce57a67f8f1d2945315c5
Gabi Desktop <gabii12303.net@gmail.com>
2023-06-29 16:29:02 -0300

    em andamento 🚧

--- principal.py
+++ principal.py
@@ -1,14 +1,8 @@
 try:
-    arquivo_contatos = open('dados/contatos.csv', encoding='latin_1'mode='w+')
-
-    for linha in arquivo_contatos:
-        print(linha, end='')
-
+    with open('dados/contatos.csv', encoding='latin_1') as arquivo_contatos:
+        for linha in arquivo_contatos:
+            print(linha, end='')
 except FileNotFoundError:
-    print('Arquivo não encontrado!')
-
+    print('Arquivo não encontrado')
 except PermissionError:
-    print('Você não tem permissão de escrita!')
-
-finally:
-    arquivo_contatos.close()
+    print('Sem permissão de escrita')
